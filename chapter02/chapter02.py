8af70d1b9b55d270cf1aa6199c8c4c2d792e2b5b
fernanda de leon <fernanda.-deleon@hotmail.com>
2024-02-02 01:39:02 +0100

    Image files

--- chapter02/chapter02.py
+++ chapter02/chapter02.py
@@ -13,3 +13,18 @@ dest = 'images/grizzly.jpg'
 download_url(ims[0], dest)
 im = Image.open(dest)
 im.to_thumb(128,128)
+
+bear_types = 'grizzly','black','teddy'
+path = Path('bears')
+if not path.exists(): path.mkdir()
+for o in bear_types: dest = (path/o)
+            dest.mkdir(exist_ok=True)
+            results = search_images_bing(key, f'{o} bear')
+            download_images(dest, urls=results.attrgot('content_url'))
+            
+fns = get_image_files(path)
+fns
+
+failed = verify_images(fns)
+
+failed
