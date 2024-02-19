9deba1ab8c7a7b3fea1e3b0ad708b005db5dd64a
fernanda de leon <fernanda.-deleon@hotmail.com>
2024-02-19 21:28:28 +0100

    Use ipywidget to classify bears

--- chapter02/chapter02.py
+++ chapter02/chapter02.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 ! [ -e /content ] && pip install -Uqq fastbook
 import fastbook
 fastbook.setup_book()
 
 from fastbook import *
+from fastai.vision.widgets import *
 
-key = os.environ.get('AZURE_SEARCH_KEY', 'XXX')
+key = os.environ.get('AZURE_SEARCH_KEY', 'X')
 
 search_images_bing
 
@@ -86,6 +86,91 @@ dls = bears.dataloaders(path)
 # Same image, different versions of the crop transform
 dls.train.show_batch(max_n=4, nrows=1, unique=True)
 
+# Data augmentation
 bears = bears.new(item_tfms=Resize(128), batch_tfms=aug_transforms(mult=2))
 dls = bears.dataloaders(path)
 dls.train.show_batch(max_n=8, nrows=2, unique=True)
+
+# Standard size 244x244 pixel
+# Train bear classifier
+bears = bears.new(
+    item_tfms=RandomResizedCrop(224, min_scale=0.5),
+    batch_tfms=aug_transforms())
+dls = bears.dataloaders(path)
+
+# Create learner and fine-tune
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(4)
+
+# Confusion matrix to measure accuracy
+# Diagonal correct classification
+
+interp = ClassificationInterpretation.from_learner(learn)
+interp.plot_confusion_matrix()
+
+interp.plot_top_losses(5, nrows=1)
+
+#hide_output
+cleaner = ImageClassifierCleaner(learn)
+cleaner
+
+# Unlink all images selected for deletion
+for idx in cleaner.delete(): cleaner.fns[idx].unlink()
+
+# Different category
+for idx,cat in cleaner.change(): shutil.move(str(cleaner.fns[idx]), path/cat)
+
+learn.export()
+
+# Check if file exists
+path = Path()
+path.ls(file_exts='.pkl')
+
+# Simulating inference
+learn_inf = load_learner(path/'export.pkl')
+
+# Filename to predict
+learn_inf.predict('images/grizzly.jpg')
+
+# Vocab, or stored list of all possible categories
+learn_inf.dls.vocab
+
+# Ipywidget
+btn_upload = widgets.FileUpload()
+btn_upload
+
+img = PILImage.create(btn_upload.data[-1])
+
+# Display
+out_pl = widgets.Output()
+out_pl.clear_output()
+with out_pl: display(img.to_thumb(128,128))
+out_pl
+
+# Get predictions
+pred,pred_idx,probs = learn_inf.predict(img)
+
+# Display
+lbl_pred = widgets.Label()
+lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+lbl_pred
+
+# Classification button
+btn_run = widgets.Button(description='Classify')
+btn_run
+
+def on_click_classify(change):
+    img = PILImage.create(btn_upload.data[-1])
+    out_pl.clear_output()
+    with out_pl: display(img.to_thumb(128,128))
+    pred,pred_idx,probs = learn_inf.predict(img)
+    lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+
+btn_run.on_click(on_click_classify)
+
+# Reset values
+btn_upload = widgets.FileUpload()
+
+VBox([widgets.Label('Select your bear!'),
+      btn_upload, btn_run, out_pl, lbl_pred])
+
